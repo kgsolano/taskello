be2cc3d0d33723a9254fc00ce8aaf8b09f9391f7
Kyle Solano <kylegsolano@gmail.com>
2022-11-23 09:07:53 -0800

    change undo order again

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,10 +19,10 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_users()
         undo_cards()
         undo_lists()
         undo_boards()
-        undo_users()
     seed_users()
     # Add other seed functions here
     seed_boards()
