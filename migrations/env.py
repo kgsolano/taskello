5d40a47698414c06cddf2e613427673e20c1611a
Kyle Solano <kylegsolano@gmail.com>
2022-11-28 16:37:32 -0800

    adjusted seeders

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,4 @@
-# existing imports
-from future import with_statement
+from __future__ import with_statement
 
 import logging
 from logging.config import fileConfig
@@ -7,10 +6,10 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from flask import current_app
+
 from alembic import context
 
-
-# add new import and environment variable
 import os
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get('SCHEMA')
