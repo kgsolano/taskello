00d20c34dcb7006c5ae7b67ac7f2a1b955034993
Kyle Solano <kylegsolano@gmail.com>
2022-11-28 16:32:47 -0800

    fixed seeders for production

--- migrations/versions/2aaa10ff355c_create_seeders_for_boards_cards_lists.py
+++ migrations/versions/2aaa10ff355c_create_seeders_for_boards_cards_lists.py
@@ -7,6 +7,9 @@ Create Date: 2022-11-22 14:27:38.516248
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -55,6 +58,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['userId'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
+        #  add an ALTER TABLE command here for each table created in the file
     # ### end Alembic commands ###
 
 
